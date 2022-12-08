bfc88bc5e7a7c05574b4a18fee20382283d110f5
juwoong <wwong7247@gmail.com>
2022-12-09 00:57:36 +0900

    Initial commit

---
+++ src/main/java/com/example/juwoongblog/JuwoongBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.juwoongblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuwoongBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuwoongBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juwoongblog/JuwoongBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juwoongblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuwoongBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
